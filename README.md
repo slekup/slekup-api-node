bcceafd4a9db19b2b4a5125cbf005f758a1e7a64
slekup <74949101+slekup@users.noreply.github.com>
2023-05-25 01:31:45 +1200

    index file

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # Slekup API
 
-A JavaScript library for interacting with Slekup API
+A JavaScript library for interacting with Slekup API.
+
+Slekup API is a powerful multi-purpose API that allows for fast and easy integration of a wide range of features and external services into your application.
+
+## Resources
+
+- [Official Documentation](https://slekup.com/dev/docs)
+- [Discord Server](https://discord.gg/)
+- [GitHub](https://github.com/slekup/slekup-api-node)
+- [NPM](https://www.npmjs.com/package/slekup-api)
+
+# Getting started
+
+## Installation
+
+```bash
+npm install slekup-api
+```
+
+## Usage
+
+Initialize the client with your token.
+
+```ts
+import { Client } from 'slekup-api';
+
+const client = new Client({
+  token: 'YOUR_TOKEN',
+  version: 1,
+});
+
+client.login().then(() => {
+  console.log('Logged in!');
+});
+```
+
+### Getting a random joke
+
+```ts
+client.jokes
+  .random()
+  .then((joke) => {
+    console.log(joke);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+```
+
+### Log debug messages
+
+```ts
+client.on('debug', (message) => {
+  console.log(message);
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,66 @@
-export const add = (a: number, b: number) => {
-  return a + b;
-};
+interface CacheConfiguration {
+  enabled?: boolean;
+  max?: number;
+  maxAge?: number;
+}
+
+interface SlekupApiOptions {
+  token: string;
+  version?: 1;
+  cache?: CacheConfiguration;
+}
+
+export class Client {
+  private token: string;
+  private version: number = 1;
+  private cache: CacheConfiguration = {
+    enabled: true,
+    max: 1000,
+    maxAge: 1000 * 60 * 60 * 24,
+  };
+
+  public constructor({ token, version, cache }: SlekupApiOptions) {
+    this.token = token;
+    if (version) this.version = version;
+    if (cache) {
+      if (cache.enabled !== undefined) this.cache.enabled = cache.enabled;
+      if (cache.max !== undefined) this.cache.max = cache.max;
+      if (cache.maxAge !== undefined) this.cache.maxAge = cache.maxAge;
+    }
+  }
+
+  public on(event: 'debug', callback: (message: string) => void): void;
+
+  public on(event: 'error', callback: (error: string) => void): void;
+
+  public on(event: 'ready', callback: () => void): void;
+
+  public on(event: 'warn', callback: (message: string) => void): void;
+
+  public on(
+    event: 'debug' | 'error' | 'ready' | 'warn',
+    callback: (() => void) | ((message?: string) => void)
+  ): void {
+    switch (event) {
+      case 'debug':
+        callback('debug');
+        break;
+      case 'error':
+        callback('error');
+        break;
+      case 'ready':
+        callback();
+        break;
+
+      case 'warn':
+        callback('warn');
+        break;
+    }
+  }
+
+  public async login() {
+    return new Promise((resolve, reject) => {
+      resolve('ok');
+    });
+  }
+}
